4ffa197c63d64b439b3ecdc829759191f7851929
jbisharaSAE <44623870+jbisharaSAE@users.noreply.github.com>
2019-07-11 15:46:13 +1000

    Unity Project

---
+++ Assets/Scripts/MyPlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyPlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalX = Input.GetAxisRaw("Horizontal");
+        float horizontalY = Input.GetAxisRaw("Vertical");
+
+        transform.Translate(horizontalX  * speed *Time.deltaTime, horizontalY *speed * Time.deltaTime, 0f);
+    }
+}
