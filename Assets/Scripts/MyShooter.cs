c6aa898c4ebada5c046330ef58780f1c2353cc0d
jbisharaSAE <44623870+jbisharaSAE@users.noreply.github.com>
2019-07-19 01:12:18 +1000

    Player Shoot

---
+++ Assets/Scripts/MyShooter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyShooter : MonoBehaviour
+{
+    public GameObject bullet1;
+    public Transform bulletSpawnPoint;
+    public float shootSpeed;
+
+    private Vector2 direction;
+
+    // Update is called once per frame
+    void Update()
+    {
+        FaceMouse();
+
+        if(Input.GetMouseButtonDown(0))
+            ShootBulletOne();
+    }
+
+    private void FaceMouse()
+    {
+        Vector3 mousePos = Input.mousePosition;
+
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        direction = new Vector2((mousePos.x - bulletSpawnPoint.position.x), (mousePos.y - bulletSpawnPoint.position.y));
+
+        bulletSpawnPoint.up = direction;
+
+        
+    }
+    private void ShootBulletOne()
+    {
+        GameObject myBullet = Instantiate(bullet1, transform.position, bulletSpawnPoint.rotation);
+        myBullet.GetComponent<Rigidbody2D>().AddForce(direction * shootSpeed); 
+
+    }
+}
--- Assets/Scripts/RotateObject.cs
+++ Assets/Scripts/RotateObject.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class RotateObject : MonoBehaviour
 {
-    public float speed;
+    public float rotSpeed;
+    public float shootSpeed;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0f, 0f, -1 * speed * Time.deltaTime);
+        //rb.velocity = Vector2.up * shootSpeed;
+        transform.Rotate(0f, 0f, -1 * rotSpeed * Time.deltaTime);
+
     }
 }
