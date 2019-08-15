add37dc3d0efa2ed2533439f3adc74d0ffaba8c9
jbisharaSAE <44623870+jbisharaSAE@users.noreply.github.com>
2019-08-16 02:56:09 +1000

    Minor

---
+++ Assets/Scripts/RemoveMesh.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveMesh : MonoBehaviour
+{
+    private Renderer myRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRenderer = GetComponent<Renderer>();
+        myRenderer.enabled = false;
+    }
+
+    
+}
